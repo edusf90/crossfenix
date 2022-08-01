ae3ce4491e18e49326a69e8db040dcaff4c43d54
Eduardo Santana <edusf0@hotmail.com>
2022-08-01 17:17:59 -0300

    first commit

---
+++ novo.js
@@ -0,0 +1,16 @@
+let age
+let city
+let skills
+let acceptCoffee
+let time
+let music
+let acceptBeer
+
+const name = 'Eduardo de Santana'
+age = 32
+city = 'São Paulo'
+skills = ['CSS', 'JS', 'HTML', 'NODEJS', 'MYSQL']
+acceptCoffee = true
+time = 'Santos FC'
+music = 'Rock and Brega'
+acceptBeer = true
